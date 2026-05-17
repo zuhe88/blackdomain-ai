2c58aeae3ef5b7529020fe9da4fd43f027a8443d
zhouyang9812 <zhouyang9812@gmail.com>
2026-05-17 21:12:02 +0800

    Create worldcupSchedule.js

---
+++ worldcupSchedule.js
@@ -0,0 +1,138 @@
+const worldCupSchedule = {
+
+  "6/12": [
+    {
+      stage: "小組賽",
+      group: "A",
+      home: "墨西哥",
+      away: "南非",
+      time: "03:00",
+      venue: "墨西哥城"
+    },
+    {
+      stage: "小組賽",
+      group: "A",
+      home: "韓國",
+      away: "捷克",
+      time: "10:00",
+      venue: "瓜達拉哈拉"
+    }
+  ],
+
+  "6/13": [
+    {
+      stage: "小組賽",
+      group: "B",
+      home: "加拿大",
+      away: "波士尼亞與赫塞哥維納",
+      time: "03:00",
+      venue: "多倫多"
+    },
+    {
+      stage: "小組賽",
+      group: "D",
+      home: "美國",
+      away: "巴拉圭",
+      time: "09:00",
+      venue: "洛杉磯"
+    }
+  ],
+
+  "6/14": [
+    {
+      stage: "小組賽",
+      group: "C",
+      home: "巴西",
+      away: "摩洛哥",
+      time: "06:00",
+      venue: "紐約/紐澤西"
+    },
+    {
+      stage: "小組賽",
+      group: "D",
+      home: "澳洲",
+      away: "土耳其",
+      time: "12:00",
+      venue: "溫哥華"
+    }
+  ],
+
+  "6/15": [
+    {
+      stage: "小組賽",
+      group: "F",
+      home: "荷蘭",
+      away: "日本",
+      time: "04:00",
+      venue: "阿靈頓"
+    },
+    {
+      stage: "小組賽",
+      group: "E",
+      home: "德國",
+      away: "庫拉索",
+      time: "01:00",
+      venue: "休士頓"
+    }
+  ],
+
+  "6/16": [
+    {
+      stage: "小組賽",
+      group: "G",
+      home: "比利時",
+      away: "埃及",
+      time: "03:00",
+      venue: "西雅圖"
+    },
+    {
+      stage: "小組賽",
+      group: "H",
+      home: "西班牙",
+      away: "維德角",
+      time: "00:00",
+      venue: "亞特蘭大"
+    }
+  ],
+
+  "6/17": [
+    {
+      stage: "小組賽",
+      group: "I",
+      home: "法國",
+      away: "塞內加爾",
+      time: "03:00",
+      venue: "紐約/紐澤西"
+    },
+    {
+      stage: "小組賽",
+      group: "J",
+      home: "阿根廷",
+      away: "阿爾及利亞",
+      time: "09:00",
+      venue: "堪薩斯城"
+    }
+  ],
+
+  "6/18": [
+    {
+      stage: "小組賽",
+      group: "L",
+      home: "英格蘭",
+      away: "克羅埃西亞",
+      time: "04:00",
+      venue: "阿靈頓"
+    },
+    {
+      stage: "小組賽",
+      group: "K",
+      home: "葡萄牙",
+      away: "剛果民主共和國",
+      time: "01:00",
+      venue: "休士頓"
+    }
+  ]
+
+};
+
+module.exports = worldCupSchedule;
