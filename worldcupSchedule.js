c864b04d0efc372f15d9546933f45567b6dba5a2
zuhe88 <zhouyang9812@gmail.com>
2026-07-02 17:19:30 +0800

    Update worldcupSchedule.js

--- worldcupSchedule.js
+++ worldcupSchedule.js
@@ -1,47 +1,40 @@
 const worldCupSchedule = {
- "7/3": [
-  { stage: "32強", group: "", home: "澳洲", away: "埃及", time: "02:00", venue: "阿靈頓" },
-  { stage: "32強", group: "", home: "阿根廷", away: "維德角", time: "06:00", venue: "邁阿密" },
-  { stage: "32強", group: "", home: "哥倫比亞", away: "迦納", time: "09:30", venue: "堪薩斯城" },
-],
-  
-  "7/4": [
-    { stage: "32強", group: "", home: "D組第2", away: "G組第2", time: "02:00", venue: "阿靈頓" },
-    { stage: "32強", group: "", home: "J組第1", away: "H組第2", time: "06:00", venue: "邁阿密" },
-    { stage: "32強", group: "", home: "K組第1", away: "D/E/I/J/L組第三", time: "09:30", venue: "堪薩斯城" },
+  "7/3": [
+    { stage: "32強", group: "", home: "西班牙", away: "奧地利", time: "03:00", venue: "洛杉磯" },
+    { stage: "32強", group: "", home: "哥倫比亞", away: "迦納", time: "09:30", venue: "堪薩斯城" },
   ],
 
-  "7/5": [
-    { stage: "16強", group: "", home: "32強勝方", away: "32強勝方", time: "01:00", venue: "休士頓" },
-    { stage: "16強", group: "", home: "32強勝方", away: "32強勝方", time: "05:00", venue: "費城" },
+  "7/4": [
+    { stage: "16強", group: "", home: "加拿大", away: "摩洛哥", time: "01:00", venue: "休士頓" },
+    { stage: "16強", group: "", home: "巴拉圭", away: "法國", time: "05:00", venue: "費城" },
   ],
 
   "7/6": [
-    { stage: "16強", group: "", home: "32強勝方", away: "32強勝方", time: "04:00", venue: "紐約/紐澤西" },
-    { stage: "16強", group: "", home: "32強勝方", away: "32強勝方", time: "08:00", venue: "墨西哥城" },
+    { stage: "16強", group: "", home: "巴西", away: "挪威", time: "04:00", venue: "紐約/紐澤西" },
+    { stage: "16強", group: "", home: "墨西哥", away: "英格蘭", time: "08:00", venue: "墨西哥城" },
   ],
 
   "7/7": [
-    { stage: "16強", group: "", home: "32強勝方", away: "32強勝方", time: "03:00", venue: "阿靈頓" },
-    { stage: "16強", group: "", home: "32強勝方", away: "32強勝方", time: "05:00", venue: "西雅圖" },
+    { stage: "16強", group: "", home: "葡萄牙/克羅埃西亞 勝方", away: "西班牙/奧地利 勝方", time: "03:00", venue: "阿靈頓" },
+    { stage: "16強", group: "", home: "美國", away: "比利時", time: "05:00", venue: "西雅圖" },
   ],
 
   "7/8": [
-    { stage: "16強", group: "", home: "32強勝方", away: "32強勝方", time: "00:00", venue: "亞特蘭大" },
-    { stage: "16強", group: "", home: "32強勝方", away: "32強勝方", time: "04:00", venue: "溫哥華" },
+    { stage: "16強", group: "", home: "阿根廷/維德角 勝方", away: "澳洲/埃及 勝方", time: "00:00", venue: "亞特蘭大" },
+    { stage: "16強", group: "", home: "瑞士/阿爾及利亞 勝方", away: "哥倫比亞/迦納 勝方", time: "04:00", venue: "溫哥華" },
   ],
 
   "7/10": [
-    { stage: "8強", group: "", home: "16強勝方", away: "16強勝方", time: "04:00", venue: "波士頓" },
+    { stage: "8強", group: "", home: "巴拉圭/法國 勝方", away: "加拿大/摩洛哥 勝方", time: "04:00", venue: "波士頓" },
   ],
 
   "7/11": [
-    { stage: "8強", group: "", home: "16強勝方", away: "16強勝方", time: "03:00", venue: "洛杉磯" },
+    { stage: "8強", group: "", home: "葡萄牙/克羅埃西亞/西班牙/奧地利 勝方", away: "美國/比利時 勝方", time: "03:00", venue: "洛杉磯" },
   ],
 
   "7/12": [
-    { stage: "8強", group: "", home: "16強勝方", away: "16強勝方", time: "05:00", venue: "邁阿密" },
-    { stage: "8強", group: "", home: "16強勝方", away: "16強勝方", time: "09:00", venue: "堪薩斯城" },
+    { stage: "8強", group: "", home: "巴西/挪威 勝方", away: "墨西哥/英格蘭 勝方", time: "05:00", venue: "邁阿密" },
+    { stage: "8強", group: "", home: "阿根廷/維德角/澳洲/埃及 勝方", away: "瑞士/阿爾及利亞/哥倫比亞/迦納 勝方", time: "09:00", venue: "堪薩斯城" },
   ],
 
   "7/15": [
